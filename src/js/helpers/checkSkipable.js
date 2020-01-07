96d83b253d5958414270c6c4be1c1b409c720eb3
Erik <bai@halfhelix.com>
2020-01-07 23:31:44 +0800

    Update

--- src/js/helpers/checkSkipable.js
+++
@@ -1,9 +0,0 @@
-export const checkSkipable = (item) => {
-  let isSkipable = false
-  item.arguments.map(argument => {
-    if (argument.name === 'button' && argument.value === 'cancel') {
-      isSkipable = true
-    }  
-  })
-  return isSkipable
-}
--- src/js/helpers/getUrl.js
+++
@@ -1,4 +0,0 @@
-export const getFileUrl = (item) => {
-  let url = item.item.file && item.item.file
-  return url
-}
